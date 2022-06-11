0684e3d6f7e883fd6fad390ae876978d21beef14
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-06-11 22:23:24 +0900

    変更した内容をメモ

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function($) {
+    $(window).scroll(function(){
+        var targetElement=$('.fadein').offset().top;
+        var scroll=$(window).scrollTop();
+        var windowHeight=$(window).height();
+        if(scroll>targetElement-windowHeight+600){
+            $('.fadein').css('opacity','1');
+            $('.fadein').css('transform','translateY(0)');
+        }
+    });
+
+});
+   
+    
