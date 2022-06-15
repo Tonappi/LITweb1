478434e57a2b10b3e6eddc3755d0c110af1378df
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-06-15 22:22:21 +0900

    変更した内容をメモ

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ $(function($) {
         var targetElement=$('.fadein').offset().top;
         var scroll=$(window).scrollTop();
         var windowHeight=$(window).height();
-        if(scroll>targetElement-windowHeight+600){
+        if(scroll>targetElement-windowHeight+90){
             $('.fadein').css('opacity','1');
             $('.fadein').css('transform','translateY(0)');
         }
     });
+
+    $(".nav_toggle").on("click", function () {
+        $(".nav_toggle, .nav").toggleClass("show");
+      });
 
 });
    
